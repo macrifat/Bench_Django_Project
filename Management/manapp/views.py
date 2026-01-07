17ab49c18b8ba726ae2347dc276b78e1d3dd006d
m.chowdhury <m.chowdhury@gmail.com>
2026-01-07 17:45:21 +0100

    connect urls&views

--- Management/manapp/views.py
+++ Management/manapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('Home View')
+def products(request):
+    return HttpResponse('Products View')
+def customer(request):
+    return HttpResponse('Customer View')
+    
